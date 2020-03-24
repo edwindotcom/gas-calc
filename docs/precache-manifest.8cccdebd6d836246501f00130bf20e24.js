f23aff7999945cb4b5b21efb3c8dce1407746cd1
Edwin Wong <edwinwongsf@gmail.com>
2020-03-24 16:26:46 -0700

    update for subdir

---
+++ docs/precache-manifest.8cccdebd6d836246501f00130bf20e24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7d94e5d73a09f7c1a5e3e025dc4d00",
+    "url": "/gas-calc/index.html"
+  },
+  {
+    "revision": "51e0472d352bfc95529f",
+    "url": "/gas-calc/static/css/main.afccbaf1.chunk.css"
+  },
+  {
+    "revision": "3409273df43074256582",
+    "url": "/gas-calc/static/js/2.ee73f4b8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gas-calc/static/js/2.ee73f4b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e0472d352bfc95529f",
+    "url": "/gas-calc/static/js/main.7f16a5e6.chunk.js"
+  },
+  {
+    "revision": "59a6a2dc5e673733dcd4",
+    "url": "/gas-calc/static/js/runtime-main.24158f45.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8981fd8dc4fa97a586ad5d9be69fc94c.js"
+  "/gas-calc/precache-manifest.8cccdebd6d836246501f00130bf20e24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gas-calc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
